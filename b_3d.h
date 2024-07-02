8c5940730e9ae31ce1d77493e4fca2190907f9d8
Benjamin <bjbrooks00@proton.me>
2024-07-02 02:26:10 -0400

    merged vec and mat to one file 3d

--- b_3d.h
+++ b_3d.h
@@ -281,3 +281,39 @@ vec2 vec4_tovec2 (vec4 v);
 /*Converts a 4 dimensional vector to a 3 dimensional one*/
 vec3 vec4_tovec3 (vec4 v);
 
+/*A column major 4x4 matrix*/
+typedef struct{
+  float elements[16];
+} mat4;
+
+extern const mat4 MAT4_IDENTITY;
+
+mat4 mat4_lookAt (vec3 eye, vec3 center, vec3 up);
+/*Perspective transformation used for calculating depth*/
+mat4 mat4_perspective (float fov, float aspect, float near, float far);
+/*Adds a 4x4 matrix to another 4x4 matrix.*/
+mat4 mat4_add (mat4 a, mat4 b);
+
+/*Returns the difference between two 4x4 matrices.*/
+mat4 mat4_subtract (mat4 min, mat4 sub);
+
+/*Prints a formatted mat4 to the console.*/
+void mat4_printf (mat4 m, const char *label);
+
+/*Scales (multiplies) a 4x4 matrix by a scalar (number)*/
+mat4 mat4_scale (const vec3 scale);
+
+/*Returns a rotation matrix around "axis" by "angle"*/
+mat4 mat4_rotate (const float angle, vec3 axis);
+
+/*Translates a vector to a 4x4 matrix*/
+mat4 mat4_translateVec3 (vec3 t);
+
+/*Translates a vector to a 4x4 matrix*/
+mat4 mat4_translateVec4 (vec4 t);
+
+/*Multiplies a 4x4 matrix with another 4x4 matrix*/
+mat4 mat4_multiply (const mat4 a, const mat4 b);
+
+/*Multiplies a 4x4 matrix with a 4 dimensional vector*/
+vec4 mat4_multiplyvec4(vec4 Left, mat4 Right);
