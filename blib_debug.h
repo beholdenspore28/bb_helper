af8d19becde46e47c741d010b3feb4a81c70ef8e
Benjamin Brooks <bjbrooks00@proton.me>
2025-01-12 19:52:10 -0500

    improved logging

--- blib_debug.h
+++ blib_debug.h
@@ -1,12 +1,23 @@
-#ifndef BLIB_DEBUG_H
-#define BLIB_DEBUG_H
+#ifndef BLIB_LOG_H
+#define BLIB_LOG_H
 
 #include <stdio.h>
 
-#define BLIB_DEBUG_STRING "[BLIB] "
+#ifndef BLIB_LOG_STRING
+#define BLIB_LOG_STRING "BLIB"
+#endif
+
+#ifndef BLIB_LOG_STREAM
 #define BLIB_LOG_STREAM stdout
+#endif
+
+#ifndef BLIB_WARNING_STREAM
 #define BLIB_WARNING_STREAM BLIB_LOG_STREAM
+#endif
+
+#ifndef BLIB_ERROR_STREAM
 #define BLIB_ERROR_STREAM stderr
+#endif
 
 #define BLIB_ANSI_COLOR_RED	"\x1b[31m"
 #define BLIB_ANSI_COLOR_GREEN	"\x1b[32m"
@@ -16,24 +27,24 @@
 #define BLIB_ANSI_COLOR_CYAN	"\x1b[36m"
 #define BLIB_ANSI_COLOR_RESET	"\x1b[0m"
 
-#define debug_log(...) {\
-	fprintf(BLIB_LOG_STREAM, BLIB_ANSI_COLOR_CYAN BLIB_DEBUG_STRING __VA_ARGS__);\
+#define blib_log(..., file, line) {\
+	fprintf(BLIB_LOG_STREAM,BLIB_ANSI_COLOR_CYAN "[" BLIB_LOG_STRING "] " __VA_ARGS__);\
 	fprintf(BLIB_LOG_STREAM, "\n" BLIB_ANSI_COLOR_RESET);\
 }
 
-#define debug_warn(...) {\
-	fprintf(BLIB_WARNING_STREAM, BLIB_ANSI_COLOR_YELLOW BLIB_DEBUG_STRING __VA_ARGS__);\
+#define blib_warn(..., file, line) {\
+	fprintf(BLIB_WARNING_STREAM, BLIB_ANSI_COLOR_YELLOW BLIB_LOG_STRING __VA_ARGS__);\
 	fprintf(BLIB_WARNING_STREAM, "\n" BLIB_ANSI_COLOR_RESET);\
 }
 
-#define debug_error(...) {\
-	fprintf(BLIB_ERROR_STREAM, BLIB_ANSI_COLOR_RED BLIB_DEBUG_STRING __VA_ARGS__);\
+#define blib_error(..., file, line) {\
+	fprintf(BLIB_ERROR_STREAM, BLIB_ANSI_COLOR_RED BLIB_LOG_STRING __VA_ARGS__);\
 	fprintf(BLIB_ERROR_STREAM, "\n" BLIB_ANSI_COLOR_RESET);\
 }
 
-#define debug_test()\
-	debug_log("this is a test message");\
-	debug_warn("this is a test warning");\
-	debug_error("this is a test error");
+#define blib_test()\
+	blib_log("this is a test message");\
+	blib_warn("this is a test warning");\
+	blib_error("this is a test error");
 
-#endif // BLIB_DEBUG_H
+#endif // BLIB_LOG_H
